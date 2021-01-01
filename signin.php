433634ce53065540ddc5be9f5b4df8bd9d2d8ea4
rexillion <61246620+rexillion@users.noreply.github.com>
2021-01-01 19:40:42 +0200

    Update signin.php

--- signin.php
+++ signin.php
@@ -16,11 +16,11 @@ echo "Connected successfully ";
 $username = $conn->real_escape_string($_REQUEST['uservalue']);
 $password = $conn->real_escape_string($_REQUEST['psw']);
 
-$result = $conn->query("SELECT * FROM users WHERE username='$username'");
+$result = $conn->query("SELECT * FROM usern WHERE name='$username'");
 
 $user = $result->fetch_assoc();
 
-$pass = sha1($password);
+$pass = $password;
 
 if ( $pass == $user['password'] ) {
     echo"<h1>welcome user ".$username."! Your email is ".$user['email']."</h1>";
